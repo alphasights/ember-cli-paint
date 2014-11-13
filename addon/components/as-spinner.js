0df5c06c49a486465079bcc067c3610ad9d44a94
Matteo Depalo <matteodepalo@gmail.com>
2014-11-13 12:10:16 +0000

    Import Spinner as an AMD module

--- addon/components/as-spinner.js
+++ addon/components/as-spinner.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
+import Spinner from 'spinjs';
 
 export default Ember.Component.extend({
   classNames: ['spinner'],
 
   didInsertElement: function() {
-    this.$().spin({
+    new Spinner({
       width: 2,
       length: 3,
       radius: 4,
       color: '#ffffff'
-    });
+    }).spin(this.$()[0]);
   }
 });
