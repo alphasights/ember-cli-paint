7ab310b9a73e5411ee7235544c866c7d35047168
Matteo Depalo <matteodepalo@gmail.com>
2014-11-13 11:19:12 +0000

    Move templates into the app namespace from the addon one

--- addon/components/as-dropdown.js
+++ addon/components/as-dropdown.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   options: function() {
-    return `align: ${this.get('align')}`;
+    return 'align: ' + this.get('align');
   }.property('align'),
 
   id: function() {
-    return `${Ember.guidFor(this)}-dropdown`;
+    return Ember.guidFor(this) + '-dropdown';
   }.property('element'),
 
   close: function() {
