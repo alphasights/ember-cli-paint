aa796c626d91f0c733513ae7f33a06aaed0dde20
Matteo Depalo <matteodepalo@gmail.com>
2015-01-07 11:19:40 +0000

    Add liquid fire to post install script

--- blueprints/ember-cli-paint/index.js
+++ blueprints/ember-cli-paint/index.js
@@ -4,11 +4,15 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackagesToProject([
-      { name: 'paint', target: '0.5.1' },
-      { name: 'spinjs', target: '2.0.1' }
+    return this.addPackagesToProject([
+      { name: 'liquid-fire', target: '0.15.0' }
     ]).then(function() {
-      return this.insertIntoFile('.jshintrc', '    "Spinner": true,', { after: '"predef": {\n' } );
+      return this.addBowerPackagesToProject([
+        { name: 'paint', target: '0.5.1' },
+        { name: 'spinjs', target: '2.0.1' }
+      ]).then(function() {
+        return this.insertIntoFile('.jshintrc', '    "Spinner": true,', { after: '"predef": {\n' } );
+      }.bind(this));
     }.bind(this));
  }
 }
