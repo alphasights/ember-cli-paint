f2090ba4a052dcecb39f8d575dddebf0c1b87579
Matteo Depalo <matteodepalo@gmail.com>
2015-01-07 10:23:17 +0000

    Update paint version

--- blueprints/ember-cli-paint/index.js
+++ blueprints/ember-cli-paint/index.js
@@ -5,7 +5,7 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'paint', target: '0.3.0' },
+      { name: 'paint', target: '0.5.1' },
       { name: 'spinjs', target: '2.0.1' }
     ]).then(function() {
       return this.insertIntoFile('.jshintrc', '    "Spinner": true,', { after: '"predef": {\n' } );
