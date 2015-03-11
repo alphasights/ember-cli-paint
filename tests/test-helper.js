73c460421f4e6d4e104d23deb03c2a37b1dff731
Matteo Depalo <matteodepalo@gmail.com>
2015-03-11 11:42:45 +0000

    Update ember-cli to 0.2.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -6,9 +6,3 @@ import {
 setResolver(resolver);
 
 Foundation.global.namespace = '';
-
-document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
-var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
-document.getElementById('ember-testing-container').style.visibility = containerVisibility;
