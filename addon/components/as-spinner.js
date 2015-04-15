7c308e1b608972c2aa635a6fe3255aaed0193069
Matteo Depalo <matteodepalo@gmail.com>
2015-04-15 12:12:04 +0100

    Use fat arrows

--- addon/components/as-spinner.js
+++ addon/components/as-spinner.js
@@ -10,9 +10,9 @@ export default Ember.Component.extend({
   width: 2,
 
   onDidInsertElement: function() {
-    Ember.run.schedule('afterRender', function() {
+    Ember.run.schedule('afterRender', () => {
       new Spinner(this.getProperties('width', 'length', 'radius', 'color'))
         .spin(this.$()[0]);
-    }.bind(this));
+    });
   }.on('didInsertElement')
 });
