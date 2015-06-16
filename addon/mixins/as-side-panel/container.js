c368064b2fd60a9f201f54690ebd799f0ef73564
Eugenio Depalo <eugenio@alphasights.com>
2015-06-16 10:54:29 +0100

    Remove wrapping div for side panel

--- addon/mixins/as-side-panel/container.js
+++ addon/mixins/as-side-panel/container.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
+  classNameBindings: ['hasFooter:has-footer', 'hasHeader:has-header'],
   hasFooter: false,
   hasHeader: false
 });
