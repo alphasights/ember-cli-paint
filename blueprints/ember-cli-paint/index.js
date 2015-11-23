01b53be746204e4404f28cca38a520befe72945e
Ju Liu <ju.liu@welaika.com>
2015-11-23 10:44:13 +0000

    Update dependencies

--- blueprints/ember-cli-paint/index.js
+++ blueprints/ember-cli-paint/index.js
@@ -5,17 +5,17 @@ module.exports = {
 
   afterInstall: function() {
     return this.addPackagesToProject([
-      { name: 'broccoli-sass', target: '0.6.6' }
+      { name: 'broccoli-sass', target: '0.7.0' }
     ]).then(function() {
       return this.addBowerPackagesToProject([
-        { name: 'paint', target: '0.9.0' },
+        { name: 'paint', target: '0.9.5' },
         { name: 'modernizr', target: '2.8.3' },
-        { name: 'spinjs', target: '2.0.1' },
+        { name: 'spinjs', target: '2.0.2' },
         { name: 'tooltipster', target: '3.3.0' },
         { name: 'trackpad-scroll-emulator', target: '1.0.8' }
       ]).then(function() {
         return this.insertIntoFile('.jshintrc', '    "Spinner",', { after: '"predef": [\n' } );
       }.bind(this));
     }.bind(this));
- }
+  }
 }
