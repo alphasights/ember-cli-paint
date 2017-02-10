5b43ef3c0bfd1ef02595c7debd7a86af297df86a
Andrei Popa <andrei.deioo@gmail.com>
2017-02-10 14:16:45 +0000

    Don't include assets in index.js anymore

--- index.js
+++ index.js
@@ -7,29 +7,5 @@ module.exports = {
 
   included: function(app) {
     this._super.included(app);
-
-    app.import(path.join(app.bowerDirectory, 'paint/styles/paint.scss'));
-    app.import(path.join(app.bowerDirectory, 'modernizr/modernizr.js'));
-    app.import(path.join(app.bowerDirectory, 'spinjs/spin.js'));
-
-    app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.ttf'), {
-      destDir: 'assets/fonts'
-    });
-
-    app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.woff'), {
-      destDir: 'assets/fonts'
-    });
-
-    app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.woff2'), {
-      destDir: 'assets/fonts'
-    });
-
-    app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.svg'), {
-      destDir: 'assets/fonts'
-    });
-
-    app.import(path.join(app.bowerDirectory, 'fontawesome/fonts/fontawesome-webfont.eot'), {
-      destDir: 'assets/fonts'
-    });
   }
 };
